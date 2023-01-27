a5140c55bd19297206ad5b59b9b996a52b6e5b0b
osama.qfnetwork <osama.qfnetwork@gmail.com>
2023-01-27 16:13:43 +0500

    webhook create

--- web/index.php
+++ web/index.php
@@ -1,8 +1,8 @@
 <?php
 
 require('../vendor/autoload.php');
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+//$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+//$dotenv->load();
 //
 //$app = new Silex\Application();
 //$app['debug'] = true;
