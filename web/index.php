350be0f9172f8bd3d9f36f0c0575134b58e6f482
osama.qfnetwork <osama.qfnetwork@gmail.com>
2023-01-27 12:27:58 +0500

    webhook create

--- web/index.php
+++ web/index.php
@@ -29,57 +29,55 @@
 //
 //$app->run();
 
-$curl = curl_init();
-
-curl_setopt_array($curl, array(
-    CURLOPT_URL => 'https://webhook.site/773d0510-b1b0-4177-984c-00d2bf99ed98',
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => 'POST',
-    CURLOPT_POSTFIELDS => '{"da":"31"}',
-    CURLOPT_HTTPHEADER => array(
-        'Content-Type: application/json'
-    ),
-));
-
-$response = curl_exec($curl);
-
-curl_close($curl);
-print_r($response);
-
 //Webhook Data
 
-$servername = "localhost";
-$username = "webhook.srisp";
-$password = "RbLHNrPjmSyfP4HD";
-$dbname = "webhook.srisp";
+//$servername = "localhost";
+//$username = "webhook.srisp";
+//$password = "RbLHNrPjmSyfP4HD";
+//$dbname = "webhook.srisp";
+//
+//// Create connection
+//$conn = new mysqli($servername, $username, $password, $dbname);
+//// Check connection
+//if ($conn->connect_error) {
+//    die("Connection failed: " . $conn->connect_error);
+//}
+if (isset($action['data']['messages'][0]['message']['extendedTextMessage']['text'])) {
+    $json = file_get_contents('php://input');
+    $action = json_decode($json, true);
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://webhook.site/773d0510-b1b0-4177-984c-00d2bf99ed98',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => $action['data'],
+        CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json'
+        ),
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
 }
-
-$json = file_get_contents('php://input');
-$action = json_decode($json, true);
-
-
-
-if (isset($action['data']['messages'][0]['message']['extendedTextMessage']['text'])){
-    $textMessage = $action['data']['messages'][0]['message']['extendedTextMessage']['text'];
-
-// sql to create table
-    $sql = "INSERT INTO webhook_data (data, number)
-VALUES ('$textMessage', '$json')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-}
+//if (isset($action['data']['messages'][0]['message']['extendedTextMessage']['text'])){
+//    $textMessage = $action['data']['messages'][0]['message']['extendedTextMessage']['text'];
+//
+//// sql to create table
+//    $sql = "INSERT INTO webhook_data (data, number)
+//VALUES ('$textMessage', '$json')";
+//
+//    if ($conn->query($sql) === TRUE) {
+//        echo "New record created successfully";
+//    } else {
+//        echo "Error: " . $sql . "<br>" . $conn->error;
+//    }
+//}
